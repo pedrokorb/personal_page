e87ec88190111d8ad0d1e67a6ff4802f73b0adb3
pedrokorb <pedro.korb@gmail.com>
2020-07-16 13:51:36 -0300

    adicionando contato link navbar

--- src/constants/links.js
+++ src/constants/links.js
@@ -1,14 +1,14 @@
 export default [
   {
-    path: "/",
-    text: "Etapas",
-  },
-  {
-    path: "/",
-    text: "LINK 2",
-  },
-  {
-    path: "/",
+    path: "https://api.whatsapp.com/send?phone=5555996737066&text=Ol%C3%A1,%20vi%20seu%20an%C3%BAncio%20no%20seu%20site%20e%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20fazer%20meu%20site%20personalizado.",
     text: "Contato",
   },
+  // {
+  //   path: "/",
+  //   text: "LINK 2",
+  // },
+  // {
+  //   path: "/",
+  //   text: "Contato",
+  // },
 ]
